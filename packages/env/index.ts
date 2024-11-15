ac0c79f8c9b2c22948952f41ce00c94ccd5e1e3c
coderreco <dev@coderre.co>
2024-11-15 18:20:17 +0100

    Refactor environment variables configuration

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,22 +1,29 @@
-import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
-const server: Parameters<typeof createEnv>[0]['server'] = {
-  CLERK_SECRET_KEY: z.string().min(1).startsWith('sk_'),
-  CLERK_WEBHOOK_SECRET: z.string().min(1).startsWith('whsec_'),
-  RESEND_AUDIENCE_ID: z.string().min(1),
+const server: Parameters<typeof createEnv>[0]["server"] = {
+  CLERK_SECRET_KEY: z
+    .string({ required_error: "sk_ or sk_test_" })
+    .min(1)
+    .startsWith("sk_" || "sk_test_"),
+  CLERK_WEBHOOK_SECRET: z.string().min(1).startsWith("whsec_"),
+  RESEND_AUDIENCE_ID: z
+    .string({
+      required_error: "Required for Resend",
+    })
+    .min(1),
   RESEND_FROM: z.string().min(1).email(),
   DATABASE_URL: z.string().min(1).url(),
-  RESEND_TOKEN: z.string().min(1).startsWith('re_'),
-  STRIPE_SECRET_KEY: z.string().min(1).startsWith('sk_'),
-  STRIPE_WEBHOOK_SECRET: z.string().min(1).startsWith('whsec_'),
+  RESEND_TOKEN: z.string().min(1).startsWith("re_"),
+  STRIPE_SECRET_KEY: z.string().min(1).startsWith("sk_"),
+  STRIPE_WEBHOOK_SECRET: z.string().min(1).startsWith("whsec_"),
   BETTERSTACK_API_KEY: z.string().min(1),
   BETTERSTACK_URL: z.string().min(1).url(),
-  ARCJET_KEY: z.string().min(1).startsWith('ajkey_'),
+  ARCJET_KEY: z.string().min(1).startsWith("ajkey_"),
   ANALYZE: z.string().optional(),
 
   // Added by Node
-  NODE_ENV: z.enum(['development', 'production']),
+  NODE_ENV: z.enum(["development", "production"]),
   CI: z.string().optional(),
 
   // Added by Sentry Integration, Vercel Marketplace
@@ -25,21 +32,25 @@ const server: Parameters<typeof createEnv>[0]['server'] = {
 
   // Added by Vercel
   VERCEL: z.string().optional(),
-  NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional(),
-  FLAGS_SECRET: z.string().min(1),
+  NEXT_RUNTIME: z.enum(["nodejs", "edge"]).optional(),
+  FLAGS_SECRET: z
+    .string({
+      required_error: "Required for Feature Flags",
+    })
+    .min(1),
 };
 
-const client: Parameters<typeof createEnv>[0]['client'] = {
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1).startsWith('pk_'),
-  NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1).startsWith('/'),
-  NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1).startsWith('/'),
-  NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().min(1).startsWith('/'),
-  NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().min(1).startsWith('/'),
+const client: Parameters<typeof createEnv>[0]["client"] = {
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1).startsWith("pk_"),
+  NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1).startsWith("/"),
+  NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1).startsWith("/"),
+  NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().min(1).startsWith("/"),
+  NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().min(1).startsWith("/"),
   NEXT_PUBLIC_APP_URL: z.string().min(1).url(),
   NEXT_PUBLIC_WEB_URL: z.string().min(1).url(),
   NEXT_PUBLIC_DOCS_URL: z.string().min(1).url(),
-  NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().min(1).startsWith('G-'),
-  NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1).startsWith('phc_'),
+  NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().min(1).startsWith("G-"),
+  NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1).startsWith("phc_"),
   NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1).url(),
 
   // Added by Vercel
